1b9bac7b9631fc7756670a4269e041e906d8c865
JisuEun <zkxktmxmfhvl@gmail.com>
2024-03-17 02:30:25 +0900

    ~ Ch4

--- src/main/java/com/example/firstproject/controller/FirstController.java
+++ src/main/java/com/example/firstproject/controller/FirstController.java
@@ -7,14 +7,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class FirstController {
     @GetMapping("/hi")
     public String niceToMeetYou(Model model) {
-        model.addAttribute("username1", "뉴휴휴");
-        model.addAttribute("username2", "돌동쉨");
+        model.addAttribute("username", "홍팍");
         return "greetings";
     }
 
     @GetMapping("/bye")
     public String seeYouNext(Model model) {
-        model.addAttribute("nickname", "민아");
-        return "GoodBye";
+        model.addAttribute("nickname", "Jisu");
+        return "goodbye";
     }
 }
