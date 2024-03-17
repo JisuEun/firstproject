c19542ec108d41f6bfe70278cd3fce021b01c7ec
JisuEun <zkxktmxmfhvl@gmail.com>
2024-03-17 13:31:48 +0900

    CRUD - Read EntityList

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import com.example.firstproject.entity.Article;
 
+import java.util.List;
 import java.util.Optional;
 
 @Slf4j
@@ -48,5 +49,15 @@ public class ArticleController {
         model.addAttribute("article", articleEntity);
         // 3. 뷰 페이지 반환하기
         return "articles/show";
+    }
+
+    @GetMapping("/articles")
+    public String index(Model model) {
+        // 1. 모든 데이터 가져오기
+        List<Article> articleEntityList = articleRepository.findAll();
+        // 2. 모델에 데이터 등록하기
+        model.addAttribute("articleList", articleEntityList);
+        // 3. 뷰 페이지 설정하기
+        return "articles/index";
     }
 }
